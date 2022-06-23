39a9f1075c94a297b9b031ecaee06dd3c004617b
sudipwadikar <107179469+sudipwadikar@users.noreply.github.com>
2022-06-23 23:15:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -243,15 +243,7 @@ resource "aws_security_group" "Allow_APP_Traffic" {
 #   ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
   }
 
-  ingress {
-    description      = "SSH"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    security_groups = ["${aws_security_group.sg-bastion.name}"]
-    #cidr_blocks      = ["0.0.0.0/0"]
-  #   ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
-  }
+
 
   egress {
     from_port        = 0
