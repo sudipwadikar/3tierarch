3faf3cbbc66129ff07c6d65be49999296476da98
sudipwadikar <107179469+sudipwadikar@users.noreply.github.com>
2022-06-23 23:05:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -218,3 +218,51 @@ resource "aws_subnet" "Private_Subnet_App2" {
     Name = "Private_Subnet_App2"
   }
 }
+# Create Security Group for Private APP subnet #
+
+resource "aws_security_group" "Allow_APP_Traffic" {
+  name        = "allow_APP_web_ssh_traffic"
+  description = "Allow inbound 22,80,443"
+  vpc_id      = aws_vpc.MFP_VPC.id
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+#   ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+#   ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    security_groups = ["${aws_security_group.sg-bastion.name}"]
+    #cidr_blocks      = ["0.0.0.0/0"]
+  #   ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+#   ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "Allow_APP_Web_SSH_Access"
+  }
+}
+
